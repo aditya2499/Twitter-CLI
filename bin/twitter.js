ba0a89df95a0660e5ce5995b962d0b726caa1b56
aditya2499 <47565671+aditya2499@users.noreply.github.com>
2020-04-17 01:59:14 +0530

    added keytar

--- bin/twitter.js
+++ bin/twitter.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log("inside twitter");
+const CredentialManager = require("../lib/credential-manager")
+
+async function main(){
+   const creds = new CredentialManager('twitter')
+   let [key , secret] = await creds.getKeyAndSecret();
+   console.log(key,secret) 
+}
+main().catch(console.error)
+//console.log("inside twitter");
